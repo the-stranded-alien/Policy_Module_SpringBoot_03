427f770543c019304dd6aa80969d0112c8bca1da
the-stranded-alien <thestrandedalien.mysticdark@gmail.com>
2021-05-31 22:26:12 +0530

    Testing Folder

---
+++ src/test/java/SpringBoot/Policy_Module_Pro/PolicyModuleProApplicationTests.java
@@ -0,0 +1,13 @@
+package SpringBoot.Policy_Module_Pro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolicyModuleProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
